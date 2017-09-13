20a50f56aa0b1d6a6c34a52fd06653af4dbcd234
Eric S <ydrive65@yahoo.com>
2017-09-13 10:56:08 -0400

    Basic implementation of Student class

---
+++ SqlInterface/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlInterface
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string City { get; set; }
+        public string StateCode { get; set; }
+        public string ZipCode { get; set; }
+        public string Email { get; set; }
+        public int MajorId { get; set; }
+        public int SatScore { get; set; }
+        public double GPA { get; set; }
+
+
+        public bool Read(SqlDataReader sqlDataReader)
+        {
+            return true;
+        }
+    }
+}
---
+++ SqlTestApp/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SqlInterface;
+
+namespace SqlTestApp
+{
+    class Program
+    {
+        // Entry point into program
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+
+        // Here we run all our program logic
+        void Run()
+        {
+            string connectionString = @"server=localhost;database=DotNetDatabase;Trusted_Connection=yes";
+            SqlConnection sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString = connectionString;
+
+            sqlConnection.Open();
+            if (sqlConnection.State != System.Data.ConnectionState.Open)
+            {
+                Console.WriteLine("Connection failed");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Connection successful");
+            }
+
+            // Do your SQL stuff here /////////////////////////////////////////////////////////////
+            string commandString = "SELECT * FROM STUDENT";
+            SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            Student student = new Student();
+
+            while (sqlDataReader.Read())
+            {
+                student.FirstName = sqlDataReader.GetString(sqlDataReader.GetOrdinal("FirstName"));
+                student.LastName = sqlDataReader.GetString(sqlDataReader.GetOrdinal("LastName"));
+                student.Id = sqlDataReader.GetInt32(sqlDataReader.GetOrdinal("Id"));
+                if (!sqlDataReader.IsDBNull(sqlDataReader.GetOrdinal("MajorID")))
+                {
+                    student.MajorId = sqlDataReader.GetInt32(sqlDataReader.GetOrdinal("MajorId"));
+                }
+                else
+                {
+                    student.MajorId = 0;
+                }
+                //Console.WriteLine(int.Parse(sqlDataReader["MajorId"].ToString()));
+            }
+            // End SQL stuff, OK to close /////////////////////////////////////////////////////////
+
+            sqlConnection.Close();
+            Console.WriteLine("Connection closed");
+        }
+    }
+}
